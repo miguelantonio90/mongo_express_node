bb28dcc0fc82a369b7e46f2762ece0523be4c814
miguelcabreja90 <miguel.cabreja90@gmail.com>
2020-05-27 10:56:53 -0400

    updated elements

--- controllers/Authentication.js
+++ controllers/Authentication.js
@@ -9,7 +9,7 @@ let login = async (req, res) => {
     return res.status(400).send({ errors: errors.array() })
   }
 
-  let { email, password } = req.body.params
+  let { email, password } = req.body
 
   try {
     let [user] = await Promise.all([User.findOne({ email })])
